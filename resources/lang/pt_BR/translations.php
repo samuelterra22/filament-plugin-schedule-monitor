2a6d47abfb3ab4bea0b90c6621133829989dbdea
Samuel Terra <samuelterra22@hotmail.com>
2024-07-02 11:23:34 -0300

    feat: add translate to pt_BR

---
+++ resources/lang/pt_BR/translations.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    'monitored_scheduled_task'          => [
+        'label'            => 'Tarefa Agendada',
+        'plural_label'     => 'Tarefas Agendadas',
+        'navigation_group' => 'Sistema',
+        'navigation_label' => 'Tarefas Agendadas',
+    ],
+    'monitored_scheduled_task_log_item' => [
+        'label'            => 'Tarefa Agendada - Logs',
+        'plural_label'     => 'Tarefas Agendadas - Logs',
+        'navigation_group' => 'Sistema',
+        'navigation_label' => 'Tarefas Agendadas - Logs',
+    ],
+    'name'                              => 'Nome',
+    'type'                              => 'Tipo',
+    'cron_expression'                   => 'Expressão Cron',
+    'timezone'                          => 'Fuso Horário',
+    'last_started_at'                   => 'Última inicialização em',
+    'last_finished_at'                  => 'Última finalização em',
+    'last_failed_at'                    => 'Última falha em',
+    'last_skipped_at'                   => 'Último ignorado em',
+    'created_at'                        => 'Criado em',
+    'memory'                            => 'Memória',
+    'runtime'                           => 'Tempo de execução',
+    'starting'                          => 'iniciando',
+    'finished'                          => 'finalizado',
+    'failed'                            => 'falhou',
+    'skipped'                           => 'ignorado',
+];
